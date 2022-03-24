ae82954206bb9752e6e36c3e603d75f9c452812f
Igor Patro <i.patro@wp.pl>
2022-03-24 22:31:37 +0100

    Added counter tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 const App = () => {
+  const [number, setNumber] = React.useState(0)
+
+  const incrementNumber = () => setNumber((prevState) => (prevState += 1))
+
   return (
     <div className="App">
       <h1>test</h1>
+      <h3>{number}</h3>
+      <button onClick={incrementNumber}>Increment</button>
     </div>
   )
 }
--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import App from "../App"
 
-it("renders 'test' heading", () => {
+it("renders element with 'test' content", () => {
   render(<App />)
+
   const headingElement = screen.getByText(/test/i)
   expect(headingElement).toBeInTheDocument()
+})
+
+it("increments counter after click", () => {
+  render(<App />)
+
+  const buttonElement = screen.getByText(/increment/i)
+  const counterElement = screen.getByText(/0/i)
+  expect(buttonElement).toBeInTheDocument()
+  expect(counterElement).toBeInTheDocument()
+
+  console.log(buttonElement)
+
+  fireEvent.click(buttonElement)
+  expect(counterElement).toHaveTextContent("1")
 })
